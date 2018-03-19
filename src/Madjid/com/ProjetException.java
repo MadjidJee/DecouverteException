cfae51d1fe030eaa11989b3de1b1e2d67fde240a
MadjidJee <madjidsebsadji@gmail.com>
2018-03-19 16:14:09 +0100

    Rajout d'une exception

--- src/Madjid/com/ProjetException.java
+++ src/Madjid/com/ProjetException.java
@@ -1,9 +1,17 @@
 package Madjid.com;
 
+import java.io.File;
+
 public class ProjetException {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try {
+			File.createTempFile("", null);
+		}catch (Exception erreur) {
+			erreur.printStackTrace();
+			
+		}
 
 	}
 
